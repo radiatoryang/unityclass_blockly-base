031eb502cf65b77e8149206784488cd100519ffe
Robert Yang <yang.robert.w@gmail.com>
2014-02-05 02:10:40 -0500

    initial commit; 2 levels, blocklyDemo and blocklyLab

---
+++ Assets/scripts/SolutionDemo.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// this script inherits from Blockly.cs instead of just MonoBehaviour
+// so it also has access to all the functions inside Blockly.cs! wow!
+
+// all Blockly *functions* start with the word "Blockly"...
+
+public class SolutionDemo : Blockly {
+
+	// FixedUpdate is called every X seconds... "X" is defined in Edit >> Project Settings >> Time
+	void FixedUpdate () {
+
+		// move forward
+		BlocklyMove ( transform.forward );
+
+		// if we detect a wall that is 2 units in front of us, then turn 45 degrees to the left
+		if ( BlocklyDetectWall ( transform.forward, 2f ) ) {
+			BlocklyTurn ( -45f );
+		}
+
+	}
+}
---
+++ Assets/scripts/SolutionLab.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// this script inherits from Blockly.cs instead of just MonoBehaviour
+// so it also has access to all the functions inside Blockly.cs! wow!
+
+// all Blockly *functions* start with the word "Blockly"...
+// BlocklyMove(), BlocklyTurn(), BlocklyDetectWall()
+
+public class SolutionLab : Blockly {
+
+	// FixedUpdate is called every X seconds... "X" is defined in Edit >> Project Settings >> Time
+	void FixedUpdate () {
+		// LAB CHALLENGE: use *ONLY* if() statements and Blockly_ functions
+		// within the FEWEST lines of code possible to solve the maze!
+	
+
+	}
+}
